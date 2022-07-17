b5dc665903d6e9cc9b0a729b33e7f985fcf99578
Your Name <you@example.com>
2022-07-17 11:23:38 +0200

    Add Date and Time And Clean JS Code

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const tempMaxValue = document.querySelector(".temp-max-value");
 const tempMinValue = document.querySelector(".temp-min-value");
 const tempDescription = document.querySelector(".temp-description-value");
 
-const cityNameSecond = document.querySelector(".city-name-second");
 const dateValueSecond = document.querySelector(".date-value-second");
 const iconSecond = document.querySelector(".icon-second");
 const tempValueSecond = document.querySelector(".temp-value-second");
@@ -14,7 +13,6 @@ const tempMaxValueSecond = document.querySelector(".temp-max-value-second");
 const tempMinValueSecond = document.querySelector(".temp-min-value-second");
 const tempDescriptionSecond = document.querySelector(".temp-description-value-second");
 
-const cityNameThird = document.querySelector(".city-name-third");
 const dateValueThird = document.querySelector(".date-value-third");
 const iconThird = document.querySelector(".icon-third");
 const tempValueThird = document.querySelector(".temp-value-third");
@@ -22,41 +20,26 @@ const tempMaxValueThird = document.querySelector(".temp-max-value-third");
 const tempMinValueThird = document.querySelector(".temp-min-value-third");
 const tempDescriptionThird = document.querySelector(".temp-description-value-third");
 
+const reculer = document.querySelector('#reculer');
+const present = document.querySelector('#present');
+const avancer = document.querySelector('#avancer');
 
-// document.getElementById("ville").addEventListener("keypress", (e) => {
-//     if (e.key === "Enter") {
-//         e.preventDefault();
-//         document.getElementById("buttonVille").click();
-//     }
-// })
+let city = "";
+let date = [];
+let iconData = "";
+let temp = "";
+let tempMax = "";
+let tempMin = "";
+let tempDsc = "";
 
 
-// function getVille() {
 
-//     const inputVal = document.getElementById("ville").value
+let iconUrl = "";
+let iconUrlSecond = "";
+let iconUrlThird = "";
 
-//     const apiKey = "96fcee721123fae8dc78175dbfe89863";
-//     // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(myWeather => generateHtml(myWeather))
-
-//     const generateHtml = data => {
-//         const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.weather[0]["icon"]}.svg`
-
-//         cityName.innerHTML = data.name;
-//         icon.innerHTML = `<img src="${iconUrl}">`;
-//         tempValue.innerHTML = data.main.temp + " °C";
-//         tempMaxValue.innerHTML = data.main.temp_max + " °C";
-//         tempMinValue.innerHTML = data.main.temp_min + " °C";
-//         tempDescription.innerHTML = data.weather[0]["description"];
-//         console.log(data)
-
-//     }
-
-// }
-
+let i = 0;
 
 document.getElementById("ville").addEventListener("keypress", (e) => {
     if (e.key === "Enter") {
@@ -66,6 +49,7 @@ document.getElementById("ville").addEventListener("keypress", (e) => {
 })
 
 document.getElementById("buttonVille").onclick = getVille2;
+
 
 function getVille2() {
 
@@ -79,32 +63,75 @@ function getVille2() {
         .then(myWeather => generateHtml(myWeather))
 
     const generateHtml = data => {
-        const iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.list[0]["weather"][0]["icon"]}.svg`
-        const iconUrlSecond = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.list[1]["weather"][0]["icon"]}.svg`
-        const iconUrlThird = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${data.list[2]["weather"][0]["icon"]}.svg`
 
-        cityName.innerHTML = data.city.name;
-        dateValue.innerHTML = data.list[0]["dt_txt"];
-        icon.innerHTML = `<img src="${iconUrl}">`;
-        tempValue.innerHTML = data.list[0]["main"]["temp"] + " °C";
-        tempMaxValue.innerHTML = data.list[0]["main"]["temp_max"] + " °C";
-        tempMinValue.innerHTML = data.list[0]["main"]["temp_min"] + " °C";
-        tempDescription.innerHTML = data.list[0]["weather"][0]["description"];
 
-        cityNameSecond.innerHTML = data.city.name;
-        dateValueSecond.innerHTML = data.list[1]["dt_txt"];
-        iconSecond.innerHTML = `<img src="${iconUrlSecond}">`;
-        tempValueSecond.innerHTML = data.list[1]["main"]["temp"] + " °C";
-        tempMaxValueSecond.innerHTML = data.list[1]["main"]["temp_max"] + " °C";
-        tempMinValueSecond.innerHTML = data.list[1]["main"]["temp_min"] + " °C";
-        tempDescriptionSecond.innerHTML = data.list[1]["weather"][0]["description"];
 
-        cityNameThird.innerHTML = data.city.name;
-        dateValueThird.innerHTML = data.list[2]["dt_txt"];
-        iconThird.innerHTML = `<img src="${iconUrlThird}">`;
-        tempValueThird.innerHTML = data.list[2]["main"]["temp"] + " °C";
-        tempMaxValueThird.innerHTML = data.list[2]["main"]["temp_max"] + " °C";
-        tempMinValueThird.innerHTML = data.list[2]["main"]["temp_min"] + " °C";
-        tempDescriptionThird.innerHTML = data.list[2]["weather"][0]["description"];
+        city = data.city.name;
+        date = data.list.map(item => { return item["dt_txt"] });
+        iconData = data.list.map(item => { return item["weather"][0]["icon"] })
+
+        temp = data.list.map(item => { return item["main"]["temp"] });
+        tempMax = data.list.map(item => { return item["main"]["temp_max"] });
+        tempMin = data.list.map(item => { return item["main"]["temp_min"] });
+        tempDsc = data.list.map(item => { return item["weather"][0]["description"] });
+
+        showHtml(i);
+        console.log(date)
+        console.log(temp)
+        console.log(tempDsc)
+        console.log(iconData)
+
     }
 }
+
+const showHtml = (item) => {
+    // FIRST CARD
+    cityName.innerHTML = city
+    dateValue.innerHTML = date[item];
+    iconUrl = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${iconData[item]}.svg`
+    icon.innerHTML = `<img src="${iconUrl}">`;
+    tempValue.innerHTML = temp[item] + " °C";
+    tempMaxValue.innerHTML = tempMax[item] + " °C";
+    tempMinValue.innerHTML = tempMin[item] + " °C";
+    tempDescription.innerHTML = tempDsc[item] + " °C";
+
+    // SECOND CARD
+    dateValueSecond.innerHTML = date[item + 1];
+    iconUrlSecond = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${iconData[item + 1]}.svg`
+    iconSecond.innerHTML = `<img src="${iconUrlSecond}">`;
+    tempValueSecond.innerHTML = temp[item + 1] + " °C";
+    tempMaxValueSecond.innerHTML = tempMax[item + 1] + " °C";
+    tempMinValueSecond.innerHTML = tempMin[item + 1] + " °C";
+    tempDescriptionSecond.innerHTML = tempDsc[item + 1] + " °C";
+
+    // THIRD CARD
+    dateValueThird.innerHTML = date[item + 2];
+    iconUrlThird = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${iconData[item + 2]}.svg`
+    iconThird.innerHTML = `<img src="${iconUrlThird}">`;
+    tempValueThird.innerHTML = temp[item + 2] + " °C";
+    tempMaxValueThird.innerHTML = tempMax[item + 2] + " °C";
+    tempMinValueThird.innerHTML = tempMin[item + 2] + " °C";
+    tempDescriptionThird.innerHTML = tempDsc[item + 2] + " °C";
+}
+
+reculer.addEventListener('click', e => {
+    if (i !== 0) {
+        i -= 3;
+        showHtml(i);
+        console.log("this is the reculer i: " + i)
+    }
+})
+present.addEventListener('click', e => {
+    i = 0;
+    showHtml(i);
+    console.log("this is the present i: " + i)
+
+})
+avancer.addEventListener('click', e => {
+    i += 3;
+    showHtml(i);
+    console.log("this is the avancer i: " + i)
+
+})
+
+
